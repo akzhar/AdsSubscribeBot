689417f299ff27a7bcb06d922b4f9b015b50f777
Akzhar <akzharik@mail.ru>
2020-04-06 13:50:29 +0300

    'fix'

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ bot.on(`message`, (msg) => {
       if (requests.hasOwnProperty(site) && utils.getObjSize(requests[site])) {
         msg += `<b>Запросы на ${site}:</b>\n`;
         for(let request in requests[site]) {
-          if (site.hasOwnProperty(request)) {
+          if (requests[site].hasOwnProperty(request)) {
             msg += `► <b>${request}</b> --> <a href="${site[request].url}">ссылка</a> (раз в ${site[request].frequency} мин., просканировано ${site[request].iterations} раз)\n`;
           }
         }
